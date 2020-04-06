44d1364e444f28210cdb31f999236c5fa34b6735
Paulo Henrique <paulogat33@gmail.com>
2020-04-06 07:28:58 -0300

    Removi Bons Dias

--- Projeto_Teste/Program.cs
+++ Projeto_Teste/Program.cs
@@ -7,10 +7,7 @@ namespace Projeto_Teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Boom Dia!");
-            Console.WriteLine("Boom Dia!");
-            Console.WriteLine("Boom Dia!");
-            Console.WriteLine("Boom Dia!");
+        
 
             Console.ReadLine();
         }
